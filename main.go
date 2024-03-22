96f9199155f92e48165b1284095d7ac23937b9b0
Sekou Samassa <sksamassa@gmail.com>
2024-03-22 14:35:08 +0500

    init setup of main.go

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "404 - Page not found")
+	})
+
+	if err := http.ListenAndServe(":4004", r); err != nil {
+		panic(err)
+	}
 }
